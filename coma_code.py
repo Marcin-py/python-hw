b14ff77325f5af1e6889014e5ea13672e2be58cb
Marcin <cinek19933@gmail.com>
2021-03-11 12:55:57 +0100

    Coma code change a last coma to and

---
+++ coma_code.py
@@ -0,0 +1,8 @@
+def list_thing(words):
+    if len(words) == 1:
+        return words[0]
+    return '{}, and {}'.format(', '.join(words[:-1]), words[-1])
+
+
+lit = ['banana', 'apple', 'oragne', 'strawberry']
+print(list_thing(lit))
